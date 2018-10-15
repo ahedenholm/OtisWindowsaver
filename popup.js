2c67a7d0b10dc58f05ada15cced886609702f045
Anders HEDENHOLM <anders.hedenholm@soprasteria.com>
2018-10-15 13:01:01 +0200

    Removed Open button

--- popup.js
+++ popup.js
@@ -1,41 +1,25 @@
 
 const openButton = document.getElementById('openButton');
 const saveButton = document.getElementById('saveButton');
-const saveAsButton = document.getElementById('saveAsButton');
 let closeAllWindowsCheckbox = document.getElementById('closeAllWindowsCheckbox');
 let presetList = document.getElementById('presetList');
 
-openButton.onclick = function (element) {
-  openPreset();
-};
 saveButton.onclick = function (element) {
   saveCurrentPreset();
 };
 saveAsButton.onclick = function (element) {
   inputPresetName();
-
 };
-
-const saveCurrentPreset = () => {
-  chrome.windows.getAll({ populate: true }, function (windows) {
-    chrome.storage.sync.set({ presets: windows }, function () {
-    });
-  });
-}
 
 const openPreset = (chosenPreset) => {
   // if (chosenPreset)
   // let presetToLoad = savedPresets.find(preset => preset.id === chosenPreset.id)
   // chrome.tabs.remove close all tabs first ? 
   let windows;
-  chrome.storage.sync.get(null, function (data) {
-    console.log('chosenPreset: ', chosenPreset);
-    console.log('data: ', data);
+  chrome.storage.local.get(null, function (data) {
     for (var key in data) {
-      console.log('key: ', key);
-      console.log('object[key]: ', data[key]);
       if (key === chosenPreset) {
-        windows = data[key];
+        windows = data[key].windows;
       }
     }
 
@@ -43,23 +27,35 @@ const openPreset = (chosenPreset) => {
       console.log('chrome.storage.get === undefined');
       return;
     }
-    console.log('chrome.storage.get ', data);
 
-    windows.forEach(window => {
-      chrome.windows.create({
-        url: window.tabs.map(tab => tab.url),
-        left: window.left,
-        top: window.top,
-        width: window.width,
-        height: window.height,
-        focused: window.focused,
-        incognito: window.incognito,
-        type: window.typ,
-        // see bug with setting state here:
-        // https://bugs.chromium.org/p/chromium/issues/detail?id=459841
-        // state: window.state
+    if (Array.isArray(windows)){
+      windows.forEach(window => {
+        chrome.windows.create({
+          url: window.tabs.map(tab => tab.url),
+          left: window.left,
+          top: window.top,
+          width: window.width,
+          height: window.height,
+          focused: window.focused,
+          incognito: window.incognito,
+          type: window.typ,
+          // see bug with setting state here:
+          // https://bugs.chromium.org/p/chromium/issues/detail?id=459841
+          // state: window.state
+        });
       });
-    });
+    } else {
+      chrome.windows.create({
+        url: windows.tabs.map(tab => tab.url),
+        left: windows.left,
+        top: windows.top,
+        width: windows.width,
+        height: windows.height,
+        focused: windows.focused,
+        incognito: windows.incognito,
+        type: windows.typ,
+      });
+    }
   });
 }
 
@@ -68,34 +64,42 @@ inputPresetName = () => {
   let presetNameButton = document.createElement("button");
   presetNameInput.setAttribute("type", "text");
   presetNameInput.setAttribute("placeholder", "Enter preset name");
-  presetNameButton.setAttribute("class", "button");
+  presetNameButton.setAttribute("class", "button width100percent");
   presetNameButton.setAttribute("id", "presetNameButton");
-  document.getElementById('presetNameButton').addEventListener('click', 
-      saveNewPreset(presetNameInput.value),
-      presetNameInput.parentNode.removeChild(presetNameInput),
-      presetNameInput.parentNode.removeChild(presetNameButton)
-      );
   saveAsButton.parentNode.appendChild(presetNameInput);
   saveAsButton.parentNode.appendChild(presetNameButton);
-
   presetNameButton.appendChild(document.createTextNode('OK'));
 
+  presetNameButton.onclick = function () {
+    saveNewPreset(presetNameInput.value);
+    presetNameInput.parentNode.removeChild(presetNameInput);
+    presetNameButton.parentNode.removeChild(presetNameButton);
+  };
+}
+
+const saveCurrentPreset = () => {
+  chrome.windows.getAll({ populate: true }, function (windows) {
+    chrome.storage.local.set({ presetstest: windows }, function () {
+    });
+  });
 }
 
 const saveNewPreset = (presetName) => {
   // if (presetList.find(preset => preset.name === presetName)
   //    prompt overwrite
+  
   chrome.windows.getAll({ populate: true }, function (windows) {
-    chrome.storage.sync.set({
+    chrome.storage.local.set({
       [presetName]: { name: presetName, windows: windows }
     }, function () {
+      let presetListItem = document.createElement("div");
+      presetListItem.className = "button width100percent";
+      presetListItem.textContent = presetName;
+      presetListItem.setAttribute("id", presetName);
+      presetList.appendChild(presetListItem);
+      presetListItem.onclick = function () {
+        openPreset(presetName);
+      }
     });
   });
-
-  let presetListItem = document.createElement("div");
-  presetListItem.className = "button";
-  presetListItem.textContent = presetName;
-  presetListItem.setAttribute("id", presetName);
-  presetListItem.setAttribute("onclick", function () { openPreset(presetName) });
-  presetList.appendChild(presetListItem);
 }
