34351409adcb431f33ee55cf44e01b53ff2f58b2
Anders HEDENHOLM <anders.hedenholm@soprasteria.com>
2018-10-16 08:44:41 +0200

    Fixed loadSavedPresets bug

--- popup.js
+++ popup.js
@@ -11,68 +11,7 @@ saveAsButton.onclick = function (element) {
   inputPresetName();
 };
 
-const openPreset = (chosenPreset) => {
-  let windows;
-  // async might cause problems here
-  if (closeAllWindowsCheckbox.checked) {
-    closeAllWindows();
-  }
-  chrome.storage.local.get(null, function (data) {
-    for (var key in data) {
-      if (key === chosenPreset) {
-        windows = data[key].windows;
-      }
-    }
-
-    if (windows === undefined) {
-      console.log('chrome.storage.get === undefined');
-      return;
-    }
-
-    if (Array.isArray(windows)) {
-      windows.forEach(window => {
-        chrome.windows.create({
-          url: window.tabs.map(tab => tab.url),
-          left: window.left,
-          top: window.top,
-          width: window.width,
-          height: window.height,
-          focused: window.focused,
-          incognito: window.incognito,
-          type: window.typ,
-          // see bug with setting state here:
-          // https://bugs.chromium.org/p/chromium/issues/detail?id=459841
-          // state: window.state
-        });
-      });
-    } else {
-      chrome.windows.create({
-        url: windows.tabs.map(tab => tab.url),
-        left: windows.left,
-        top: windows.top,
-        width: windows.width,
-        height: windows.height,
-        focused: windows.focused,
-        incognito: windows.incognito,
-        type: windows.typ,
-      });
-    };
-  });
-}
-
-closeAllWindows = () => {
-   chrome.windows.getAll(function (windows) {
-    if (Array.isArray(windows)) {
-      windows.forEach(window => {
-        chrome.windows.remove(window.id)
-      })
-    } else {
-      chrome.windows.remove(windows.id)
-    }
-  });
-}
-
-inputPresetName = () => {
+const inputPresetName = () => {
   let presetNameInput = document.createElement("input");
   let presetNameButton = document.createElement("button");
   presetNameInput.setAttribute("type", "text");
@@ -99,14 +38,7 @@ const saveNewPreset = (presetName) => {
     chrome.storage.local.set({
       [presetName]: { name: presetName, windows: windows, isPreset: true, }
     }, function () {
-      let presetListItem = document.createElement("div");
-      presetListItem.className = "button width100percent";
-      presetListItem.textContent = presetName;
-      presetListItem.setAttribute("id", presetName);
-      presetList.appendChild(presetListItem);
-      presetListItem.onclick = function () {
-        openPreset(presetName);
-      }
+      createPresetListItem(presetName);
     });
   });
 }
@@ -115,16 +47,60 @@ const loadSavedPresets = () => {
   chrome.storage.local.get(null, function (data) {
     for (var key in data) {
       if (data[key].isPreset) {
-        let presetListItem = document.createElement("div");
-        presetListItem.className = "button width100percent";
-        presetListItem.textContent = data[key].name;
-        presetListItem.setAttribute("id", data[key].name);
-        presetList.appendChild(presetListItem);
-        presetListItem.onclick = function () {
-          openPreset(data[key].name);
-        }
+        createPresetListItem(data[key].name);
       }
     }
   });
 }
 
+const createPresetListItem = (presetName) => {
+  let presetListItem = document.createElement("div");
+  presetListItem.className = "button width100percent";
+  presetListItem.textContent = presetName;
+  presetListItem.setAttribute("id", presetName);
+  presetListItem.addEventListener('click', () => openPreset(presetName));
+  presetList.appendChild(presetListItem);
+};
+
+const openPreset = (chosenPreset) => {
+  let windows;
+  // async might cause problems here
+  if (closeAllWindowsCheckbox.checked) {
+    closeAllWindows();
+  }
+  chrome.storage.local.get(null, function (data) {
+    for (var key in data) {
+      if (key === chosenPreset) {
+        windows = data[key].windows;
+      }
+    }
+
+    if (windows === undefined) {
+      return;
+    }
+
+    windows.forEach(window => {
+      chrome.windows.create({
+        url: window.tabs.map(tab => tab.url),
+        left: window.left,
+        top: window.top,
+        width: window.width,
+        height: window.height,
+        focused: window.focused,
+        incognito: window.incognito,
+        type: window.type,
+        // see bug with setting state here:
+        // https://bugs.chromium.org/p/chromium/issues/detail?id=459841
+        // state: window.state
+      });
+    });
+  });
+}
+
+const closeAllWindows = () => {
+  chrome.windows.getAll(function (windows) {
+    windows.forEach(window => {
+      chrome.windows.remove(window.id)
+    })
+  });
+}
